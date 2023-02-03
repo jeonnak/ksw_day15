96293f7a608a16e87ea106e4bd67f57b32ecfb10
JNK <deal6789@naver.com>
2023-02-03 16:20:43 +0900

    linked list 09

--- main.py
+++ main.py
@@ -1,55 +1,40 @@
-## 함수 선언 부분 ##
-# 다항식 형식을 출력해보겠습니다.
-# 매개변수 px는 다항식 계수 배열
-def print_poly(px, tx):
-    """
-    다항식을 포맷에 맞게 출력하는 함수
-    :param px: 계수를 원소로 가지고 있는 list
-    :param tx: 차수를 원소로 가지고 있는 list
-    :return: 다항식 문자열
-    """
-    poly_str = "P(x) = "
+## 클래스와 함수 선언 부분 ##
+class Node:
+	def __init__(self) :
+		self.data = None
+		self.link = None
 
-    for i in range(len(px)):
-        term = tx[i]  # 메모리 하나 더 써주기
-        coef = px[i]  # 계수를 추출합니다.
-        if coef >= 0:
-            poly_str = poly_str + "+"
-
-        poly_str = poly_str + f'{coef}x^{term} '
-        term = term - 1
-
-    return poly_str  # 생성된 다항식 문자열 반환합니다.
-
-
-def calc_poly(x_val, px, tx):
-    """
-    다항식의 산술연산을 계산하는 함수
-    :param x_val: x값 integer
-    :param px: 계수를 원소로 가지고 있는 list
-    :param tx: 차수를 원소로 가지고 있는 list
-    :return: 다항식 계산 결과 값 integer
-    """
-
-    return_val = 0
-
-    for i in range(len(px)):
-        term = tx[i]
-        coef = px[i]
-        return_val = return_val + coef * x_val ** term
-        term = term - 1
-
-    return return_val
-
+def print_nodes(start) :
+	current = start
+	if current == None :
+		return
+	print(current.data, end=' ')
+	while current.link != None:
+		current = current.link  # 다음 노드로 이동
+		print(current.data, end=' ')
+	print()
 
 ## 전역 변수 선언 부분 ##
-px = [3, -4, 5]
-tx = [300, 20, 0]
+memory = []
+head, current, pre = None, None, None
+dataArray = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
 
 ## 메인 코드 부분 ##
 if __name__ == "__main__":
-    print(print_poly(px, tx))
 
-    x_value = int(input("X 값 : "))
-    print(calc_poly(x_value, px, tx))
+	node = Node()		# 첫 번째 노드
+	node.data = dataArray[0]
+	head = node
+	memory.append(node)
 
+	for data in dataArray[1:] :	# 두 번째 이후 노드
+		pre = node
+		node = Node()
+		node.data = data
+		pre.link = node  # 이 부분이 핵심. 이전 노드에 새 부분을 연결하는 과정.
+		memory.append(node)
+
+	print_nodes(head)
+    print(memory)
+    print(node.data)
+    print(pre.data)
