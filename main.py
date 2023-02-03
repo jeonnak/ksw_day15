71051e1e491b04062b42167022f36e278cd6d871
JNK <deal6789@naver.com>
2023-02-03 11:30:58 +0900

    linear list 03

--- main.py
+++ main.py
@@ -11,10 +11,29 @@ def insert_data(idx, pokemon):
 
     pokemons[idx] = pokemon
 
+
+# 지우는 위치 idx만 알려주면 됨.
+def delete_data(idx):
+    if idx < 0 or idx > len(pokemons):
+        print("데이터를 삭제할 범위를 벗어났습니다.")
+        return
+
+    len_pokemons = len(pokemons)
+    pokemons[idx] = None  # 데이터 삭제
+
+    for i in range(idx + 1, len_pokemons):
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
+
+    del (pokemons[len_pokemons - 1])
+
+
 if __name__ == "__main__":
     pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
     print(pokemons)
-    insert_data(3, '거북왕')
+    delete_data(1)
     print(pokemons)
-    insert_data(6, '어니부기')
+    delete_data(3)
     print(pokemons)
+    # delete_data(5) 범위를 벗어나기 때문에 에러가 날 것임.
+    # print(pokemons)
